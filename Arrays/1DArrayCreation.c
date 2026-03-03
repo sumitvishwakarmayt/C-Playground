43084619bb924e7cae86cd432f929229caf86922
Sumit Vishwakarma <sumitvishwakarma.tech@gmail.com>
2026-03-03 20:54:06 +0530

    Removed Unwanted Files

--- Arrays/1DArrayCreation.c
+++ Arrays/1DArrayCreation.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
  void main(){
-    printf("Just a Simple Array Having 5 Elements.");
-    char name[5] = {'S','u','m','i','t'};
-    printf(name);
-    scanf("%d");
+   //  printf("Just a Simple Array Having 5 Elements.");
+   //  char name[5] = {'S','u','m','i','t'};
+   //  printf(name);
+   //  scanf("%d");
  }
--- Stacks/StackCreationProgram.c
+++
@@ -1,6 +0,0 @@
-#include<stdio.h>
-
-void main(){
-    printf("Program to Create (Implement) Stacks using Arrays");
-
-}
