92f80b3b84dcef7754c2a8e91faab1a5fc8d4af7
Sumit Vishwakarma <sumitvishwakarma.tech@gmail.com>
2026-03-06 19:51:52 +0530

    Added while loop in oddEven Program

--- Practice Programs/oddEven.c
+++ Practice Programs/oddEven.c
@@ -1,12 +1,18 @@
-void main(){
+void main()
+{
     int num;
-    printf("Enter the number to check for odd or even:");
-    scanf("%d", &num);
-    if (num%2 == 0){
-        printf("The entered number %d is even.", num);
-    }
-    else{
-        printf("The entered number %d is odd.", num);
+    while (1)
+    {
+        printf("\nEnter the number to check for odd or even:");
+        scanf("%d", &num);
+        if (num % 2 == 0)
+        {
+            printf("The entered number %d is even.", num);
+        }
+        else
+        {
+            printf("The entered number %d is odd.", num);
+        }
     }
     getch();
 }
