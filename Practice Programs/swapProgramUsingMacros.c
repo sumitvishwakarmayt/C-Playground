d6748e53764a0dd2fcb6c36336a687de611db1f8
Sumit Vishwakarma <sumitvishwakarma.tech@gmail.com>
2026-03-10 22:28:59 +0530

    Swap Program

--- Practice Programs/swapProgramUsingMacros.c
+++ Practice Programs/swapProgramUsingMacros.c
@@ -3,9 +3,12 @@
 #define swap(a,b) {int temp=a; a=b; b=temp;}
 
 main(){
-    int x=5, y=10;
+    int x,y;
+    printf("Enter two numbers to store in variable X and Y :");
+    scanf("%d%d", &x, &y);
+    printf("The initial value of X is %d.\nThe initial value of Y is %d. \n", x, y);
     swap(x,y);
-    printf("%d   %d", x,y);
+    printf("The value of X after swap is %d.\nThe value of Y after swap is %d.", x, y);
 
     getch();
 }
